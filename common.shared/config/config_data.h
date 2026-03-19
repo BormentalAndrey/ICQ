dbddce194ee94bdd0057e3e6c81bb1f5dd934a3a
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-19 21:59:23 +0300

    Update config_data.h

--- common.shared/config/config_data.h
+++ common.shared/config/config_data.h
@@ -6,4 +6,7 @@ namespace config
 {
     // Функция для получения JSON конфигурации из файла
     std::string config_json();
+    
+    // Функция для загрузки конфигурации из указанного пути
+    std::string load_config_from_file(const std::string& path);
 }
