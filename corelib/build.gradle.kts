717cd7dd65eceaf01e37c590064fd2668fed0841
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-19 15:06:03 +0300

    Create build.gradle.kts

---
+++ corelib/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.icq.corelib"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++17 -fexceptions -frtti -D__linux__ -DANDROID"
+                abiFilters.add("arm64-v8a")
+            }
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":common.shared"))
+    implementation("androidx.core:core-ktx:1.12.0")
+}
