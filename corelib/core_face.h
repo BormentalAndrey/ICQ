df904b31a1481f1df06ad06e95ed1ef7b0c49a5f
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-21 15:56:15 +0300

    Update core_face.h

--- corelib/core_face.h
+++ corelib/core_face.h
@@ -1,138 +1,140 @@
-#pragma once
+// app/src/main/cpp/jni_bridge.cpp
+#include <jni.h>
+#include <string>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <codecvt>
+#include <locale>
+#include <android/log.h>
 
-#include "ibase.h"
+#include "core/stdafx.h"
+#include "gui/core_dispatcher.h"  // core_dispatcher.h находится в папке gui/
+#include "core/icore_interface.h"  // теперь есть
 
-#include "ivalue.h"
+#define LOG_TAG "IcqCoreJNI"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-#include "../common.shared/common_defs.h"
+JavaVM* g_jvm = nullptr;
+static std::unique_ptr<Ui::core_dispatcher> g_core;  // используем Ui::core_dispatcher
+static jobject g_event_callback_obj = nullptr;
+static std::shared_ptr<core::icore_interface> g_gui_callback;
+static std::mutex g_core_mutex;
 
-namespace core
-{
-    enum login_error
-    {
-        le_success = 0,
-        le_wrong_login = 1,
-        le_network_error = 2,
-        le_parse_response = 3,
-        le_rate_limit = 4,
-        le_unknown_error = 5,
-        le_invalid_sms_code = 6,
-        le_error_validate_phone = 7,
-        le_attach_error_busy_phone = 8,
-        le_wrong_login_2x_factor = 9,
-    };
+class AndroidGuiCallback : public core::icore_interface {
+public:
+    // Реализуем все методы из icore_interface
+    void receive_variable(const std::string& _name, core::coll_ptr _value) override {
+        notify_java_event(_name);
+    }
 
-    enum class mchat_error
-    {
-        me_success = 0,
-        me_unknown_error = 1,
-        me_cannot_add = 2,
-    };
+    void receive_package(const std::string& _name, core::coll_ptr _value) override {
+        notify_java_event(_name);
+    }
 
-    enum avatar_error
-    {
-        ae_success = 0,
-        ae_network_error = 1,
-        ae_unknown_error = 2
-    };
+    void on_voip_proto_msg(const std::string& _account, const std::vector<char>& _data) override {
+        notify_java_event("voip_proto_msg");
+    }
 
-    enum collection_value_type
-    {
-        vt_empty,
-        vt_int,
-        vt_double,
-        vt_string,
-        vt_bool,
-        vt_collection,
-        vt_stream,
-        vt_array,
-        vt_int64,
-        vt_hheaders,
-        vt_uint
-    };
+    void send_statistic_event(const std::string& _event, const core::event_props_type& _props) override {
+        LOGI("Core Statistic Event: %s", _event.c_str());
+    }
 
-    struct istream : ibase
-    {
-        virtual uint8_t* read(int64_t) = 0;
-        virtual void write(std::istream& _source) = 0;
-        virtual void write(const uint8_t*, int64_t) = 0;
-        virtual bool empty() const = 0;
-        virtual int64_t size() const = 0;
-        virtual void reset() = 0;
-        virtual ~istream() {}
-    };
+    std::string get_device_id() override {
+        return "android_id";
+    }
 
-    struct iarray : ibase
-    {
-        using size_type = int32_t;
+    // Дополнительные методы из core_face.h::icore_interface
+    core::iconnector* get_core_connector() override { return nullptr; }
+    core::iconnector* get_gui_connector() override { return nullptr; }
+    core::icore_factory* get_factory() override { return nullptr; }
 
-        virtual void push_back(ivalue*) = 0;
-        virtual const ivalue* get_at(size_type) const = 0;
-        virtual void reserve(size_type) = 0;
-        virtual size_type size() const = 0;
-        virtual bool empty() const = 0;
-    };
+private:
+    void notify_java_event(const std::string& _event_name) {
+        if (!g_jvm || !g_event_callback_obj) return;
 
-    struct hheader
-    {
-        int64_t        id_;
-        int64_t        prev_id_;
-        uint64_t    time_;
-    };
+        JNIEnv* env = nullptr;
+        bool attached = false;
+        jint res = g_jvm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6);
+        
+        if (res == JNI_EDETACHED) {
+            if (g_jvm->AttachCurrentThread(&env, nullptr) != JNI_OK) {
+                LOGE("Failed to attach thread to JVM");
+                return;
+            }
+            attached = true;
+        }
 
-    struct ihheaders_list : ibase
-    {
-        virtual void push_back(hheader*) = 0;
-        virtual hheader* first() = 0;
-        virtual hheader* next() = 0;
-        virtual int32_t count() const = 0;
-        virtual bool empty() const = 0;
-    };
+        jclass callbackClass = env->GetObjectClass(g_event_callback_obj);
+        jmethodID methodId = env->GetMethodID(callbackClass, "onCoreEvent", "(Ljava/lang/String;)V");
+        
+        if (methodId) {
+            jstring jName = env->NewStringUTF(_event_name.c_str());
+            env->CallVoidMethod(g_event_callback_obj, methodId, jName);
+            env->DeleteLocalRef(jName);
+        }
+        
+        env->DeleteLocalRef(callbackClass);
+        if (attached) {
+            g_jvm->DetachCurrentThread();
+        }
+    }
+};
 
-    struct icollection : ibase
-    {
-        virtual bool is_value_exist(std::string_view name) const = 0;
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
+    g_jvm = vm;
+    return JNI_VERSION_1_6;
+}
 
-        virtual ivalue* create_value() = 0;
-        virtual icollection* create_collection() = 0;
-        virtual iarray* create_array() = 0;
-        virtual istream* create_stream() = 0;
-        virtual ihheaders_list* create_hheaders_list() = 0;
+extern "C" JNIEXPORT void JNICALL
+Java_com_icq_mobile_core_IcqCoreEngine_nativeInit(JNIEnv *env, jobject thiz, jstring data_path, jstring cache_path, jobject callback) {
+    std::lock_guard<std::mutex> lock(g_core_mutex);
+    
+    if (g_core) {
+        LOGI("Core Engine is already initialized.");
+        return;
+    }
 
-        virtual void set_value(std::string_view name, ivalue* value) = 0;
-        virtual ivalue* get_value(std::string_view name) const = 0;
-        virtual ivalue* first() = 0;
-        virtual ivalue* next() = 0;
-        virtual int32_t count() const = 0;
-        virtual bool empty() const = 0;
+    if (g_event_callback_obj) {
+        env->DeleteGlobalRef(g_event_callback_obj);
+    }
+    g_event_callback_obj = env->NewGlobalRef(callback);
+    
+    auto callback_impl = std::make_shared<AndroidGuiCallback>();
+    g_gui_callback = std::static_pointer_cast<core::icore_interface>(callback_impl);
 
-        virtual const char* log() const = 0;
+    const char *data_path_cstr = env->GetStringUTFChars(data_path, nullptr);
+    std::string path_str(data_path_cstr);
+    env->ReleaseStringUTFChars(data_path, data_path_cstr);
 
-        virtual ~icollection() {}
-    };
+    common::core_gui_settings settings;
+    settings.os_version_ = "Android";
+    settings.locale_ = "ru_RU";
+    settings.recents_avatars_size_ = 96;  // установим значение по умолчанию
+    
+    g_core = std::make_unique<Ui::core_dispatcher>();
+    g_core->link_gui(g_gui_callback, settings);
+    
+    LOGI("Core Engine initialized. Path: %s", path_str.c_str());
+}
 
-    struct iconnector : ibase
-    {
-        virtual void link(iconnector*, const common::core_gui_settings&) = 0;
-        virtual void unlink() = 0;
-        virtual void receive(std::string_view, int64_t, core::icollection*) = 0;
-
-        virtual ~iconnector() {}
-
-    };
-
-    struct icore_factory : ibase
-    {
-        virtual icollection* create_collection() = 0;
-        ~icore_factory() {}
-    };
-
-    struct icore_interface : ibase
-    {
-        virtual iconnector* get_core_connector() = 0;
-        virtual iconnector* get_gui_connector() = 0;
-        virtual icore_factory* get_factory() = 0;
-
-        virtual ~icore_interface() {}
-    };
+extern "C" JNIEXPORT void JNICALL
+Java_com_icq_mobile_core_IcqCoreEngine_nativeShutdown(JNIEnv *env, jobject thiz) {
+    std::lock_guard<std::mutex> lock(g_core_mutex);
+    
+    if (g_core) {
+        g_core->unlink_gui();
+        g_core.reset();
+        LOGI("Core Engine unlinked and reset.");
+    }
+    
+    if (g_event_callback_obj) {
+        env->DeleteGlobalRef(g_event_callback_obj);
+        g_event_callback_obj = nullptr;
+    }
+    
+    g_gui_callback.reset();
+    LOGI("Core Engine shutdown complete.");
 }
