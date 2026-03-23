2afd887dc69cb0135a6a8a9975272c78a18da2e2
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-23 20:37:18 +0300

    Update build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,6 @@ android {
     namespace = "com.icq.core"
     compileSdk = 34
     
-    // Указываем конкретную версию NDK из ваших логов для стабильности CI
     ndkVersion = "26.1.10909125"
 
     defaultConfig {
@@ -16,24 +15,24 @@ android {
 
         externalNativeBuild {
             cmake {
-                // Основные флаги компиляции
                 cppFlags += "-std=c++17 -fexceptions -frtti -D__linux__ -DANDROID"
                 
-                // Передаем аргументы для корректного поиска Qt, если пути заданы в окружении
+                // Находим путь к Qt внутри вашего репозитория
+                val qtPath = file("${project.rootDir}/Qt/android_arm64_v8a").absolutePath
+                
+                // Передаем этот путь в CMake
+                arguments += "-DQT_ANDROID_PATH=$qtPath"
                 arguments += "-DANDROID_STL=c++_shared"
                 
                 abiFilters.add("arm64-v8a")
             }
         }
         
-        // Настройки для ProGuard в библиотеке
         consumerProguardFiles("consumer-rules.pro")
     }
 
     externalNativeBuild {
         cmake {
-            // ИСПРАВЛЕНИЕ: указываем путь к реальному файлу в папке App
-            // Так как мы находимся в папке 'core', поднимаемся на уровень выше
             path = file("../app/src/main/cpp/CMakeLists.txt")
             version = "3.22.1"
         }
@@ -60,11 +59,9 @@ android {
 }
 
 dependencies {
-    // Внутренние модули проекта
     implementation(project(":corelib"))
     implementation(project(":common.shared"))
     
-    // Основные зависимости
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
