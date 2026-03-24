0d8c8b53d88b941e6c7b9c966006a0ce4e06f909
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-24 13:23:33 +0300

    Update fetch_event_typing.cpp

--- core/connections/wim/events/fetch_event_typing.cpp
+++ core/connections/wim/events/fetch_event_typing.cpp
@@ -2,7 +2,7 @@
 
 #include "fetch_event_typing.h"
 #include "../wim_im.h"
-#include "../../../../common.shared/json_helper.h"
+#include "../../../../common.shared/json_unserialize_helpers.h"
 
 using namespace core;
 using namespace wim;
@@ -14,22 +14,22 @@ fetch_event_typing::~fetch_event_typing() = default;
 int32_t fetch_event_typing::parse(const rapidjson::Value& _node_event_data)
 {
     if (const auto it = _node_event_data.FindMember("typingStatus"); it != _node_event_data.MemberEnd() && it->value.IsString())
-        is_typing_ = rapidjson_get_string_view(it->value) == "typing";
+        is_typing_ = common::json::rapidjson_get_string_view(it->value) == "typing";
 
     if (const auto it = _node_event_data.FindMember("aimId"); it != _node_event_data.MemberEnd() && it->value.IsString())
-        aimid_ = rapidjson_get_string(it->value);
+        aimid_ = common::json::rapidjson_get_string(it->value);
     else
         return -1;
 
     if (const auto it_attr = _node_event_data.FindMember("MChat_Attrs"); it_attr != _node_event_data.MemberEnd() && it_attr->value.IsObject())
     {
         if (const auto it = it_attr->value.FindMember("sender"); it != it_attr->value.MemberEnd() && it->value.IsString())
-            chatter_aimid_ = rapidjson_get_string(it->value);
+            chatter_aimid_ = common::json::rapidjson_get_string(it->value);
 
         if (const auto itoptions = it_attr->value.FindMember("options"); itoptions != it_attr->value.MemberEnd() && itoptions->value.IsObject())
         {
             if (const auto itname = itoptions->value.FindMember("senderName"); itname != itoptions->value.MemberEnd() && itname->value.IsString())
-                chatter_name_ = rapidjson_get_string(itname->value);
+                chatter_name_ = common::json::rapidjson_get_string(itname->value);
         }
     }
 
