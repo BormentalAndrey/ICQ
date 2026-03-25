ecc7acaa7defa5835edb6711b2cdee5126810a58
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-25 21:36:22 +0300

    Update config_data.h

--- common.shared/config/config_data.h
+++ common.shared/config/config_data.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <string>
+#include <string_view>
 #include "config.h"
 
 namespace config
 {
-    std::string config_json();
+    std::string_view config_json() noexcept;
     std::string load_config_from_file(const std::string& path);
 }
