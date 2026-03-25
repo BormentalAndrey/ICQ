dfa40ad9fcff4a37f70e18ee6014aaa8a9760545
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-25 23:01:51 +0300

    Update json_helper.h

--- common.shared/json_helper.h
+++ common.shared/json_helper.h
@@ -1,39 +1,49 @@
 #pragma once
+
 #include <QDataStream>
 #include <QString>
 #include <vector>
 #include <string>
+#include <variant>
+#include <optional>
 
-namespace core {
-namespace tools {
-namespace json_helper {
+// Добавляем инклуды RapidJSON здесь, чтобы они были доступны везде
+#include <rapidjson/document.h>
 
-template<typename T>
-void serialize(QDataStream& out, const T& value) { out << value; }
+namespace core::tools::json_helper
+{
+    // Сериализация базовых типов
+    template<typename T>
+    void serialize(QDataStream& out, const T& value) { out << value; }
 
-template<typename T>
-void deserialize(QDataStream& in, T& value) { in >> value; }
+    template<typename T>
+    void deserialize(QDataStream& in, T& value) { in >> value; }
 
-inline void serialize(QDataStream& out, const std::string& value) {
-    out << QString::fromStdString(value);
-}
+    // Специализация для std::string
+    inline void serialize(QDataStream& out, const std::string& value) {
+        out << QString::fromStdString(value);
+    }
 
-inline void deserialize(QDataStream& in, std::string& value) {
-    QString tmp; in >> tmp; value = tmp.toStdString();
-}
+    inline void deserialize(QDataStream& in, std::string& value) {
+        QString tmp; in >> tmp; 
+        value = tmp.toStdString();
+    }
 
-template<typename T>
-void serialize(QDataStream& out, const std::vector<T>& vec) {
-    out << (int)vec.size();
-    for (const auto& item : vec) serialize(out, item);
-}
+    // Специализация для std::vector
+    template<typename T>
+    void serialize(QDataStream& out, const std::vector<T>& vec) {
+        out << static_cast<int>(vec.size());
+        for (const auto& item : vec) serialize(out, item);
+    }
 
-template<typename T>
-void deserialize(QDataStream& in, std::vector<T>& vec) {
-    int size; in >> size; vec.resize(size);
-    for (int i = 0; i < size; ++i) deserialize(in, vec[i]);
-}
+    template<typename T>
+    void deserialize(QDataStream& in, std::vector<T>& vec) {
+        int size; in >> size; 
+        vec.resize(size);
+        for (int i = 0; i < size; ++i) deserialize(in, vec[i]);
+    }
 
-} // namespace json_helper
-} // namespace tools
-} // namespace core
+    // Объявление функции сортировки (реализация в .cpp)
+    void sort_json_keys_by_name(rapidjson::Value& _node);
+
+} // namespace core::tools::json_helper
