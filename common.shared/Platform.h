6b94391b440684e2de363bcc3f15233d81463fef
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-30 19:11:28 +0300

    Update Platform.h

--- common.shared/Platform.h
+++ common.shared/Platform.h
@@ -11,4 +11,36 @@
     #define PLATFORM_MAC 1
 #endif
 
+// Подмена WinAPI типов для кроссплатформенной сборки (в т.ч. Android)
+#ifndef PLATFORM_WIN
+    #include <stdint.h>
+    #include <stddef.h>
+
+    typedef uint32_t DWORD;
+    typedef uint16_t WORD;
+    typedef uint8_t BYTE;
+    typedef void* HANDLE;
+    typedef void* HWND;
+    typedef void* LPVOID;
+    typedef int BOOL;
+    typedef uintptr_t WPARAM;
+    typedef intptr_t LPARAM;
+    typedef long LONG;
+    typedef unsigned int UINT;
+
+    #ifndef TRUE
+        #define TRUE 1
+    #endif
+    #ifndef FALSE
+        #define FALSE 0
+    #endif
+
+    #define WINAPI
+    #define CALLBACK
+
+    // Базовая эмуляция Sleep для POSIX-систем
+    #include <unistd.h>
+    #define Sleep(x) usleep((x) * 1000)
 #endif
+
+#endif // PLATFORM_H
