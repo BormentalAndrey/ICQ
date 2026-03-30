f24bd8d873bf416ccdf06b4f051d86d93aae1ea8
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-30 19:50:59 +0300

    Update Types.h

--- gui/spellcheck/Types.h
+++ gui/spellcheck/Types.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#include <vector>
+#include <utility>
+#include <QString>
+
 namespace spellcheck
 {
     using Suggests = std::vector<QString>;
     struct WordStatus
     {
-        bool isCorrect;
-        bool isAdded;
+        bool isCorrect = false;
+        bool isAdded = false;
     };
     using MisspellingRange = std::pair<int, int>;
     using Misspellings = std::vector<MisspellingRange>;
