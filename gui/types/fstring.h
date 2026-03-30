2409cf40f00f32d1306225dbe46da8763b761792
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-03-30 21:29:48 +0300

    Update fstring.h

--- gui/types/fstring.h
+++ gui/types/fstring.h
@@ -15,7 +15,89 @@ namespace Data
     Q_DECLARE_FLAGS(FormatTypes, core::data::format_type)
     Q_DECLARE_OPERATORS_FOR_FLAGS(FormatTypes)
 
-    class FStringView;
+    // Предварительное объявление FString и его внутренних типов
+    class FString;
+    struct SharedData; // Для FString::SharedData
+
+    //! NB: FormattedStringView doesn't take care of keeping any ownership
+    class FStringView
+    {
+    public:
+        using const_iterator = QString::const_iterator;
+        using const_reverse_iterator = QString::const_reverse_iterator;
+
+        FStringView() noexcept;
+        FStringView(const FStringView&) = default;
+        FStringView(FStringView&&) noexcept = default;
+        FStringView(const FString& _string, int _offset = 0, int _size = -1);
+        ~FStringView() = default;
+
+        FStringView& operator=(const FStringView& _other) = default;
+        FStringView& operator=(FStringView&& _other) noexcept = default;
+
+        bool hasFormatting() const noexcept;
+        bool containsFormat(core::data::format_type _type) const;
+        bool isEmpty() const noexcept;
+        int size() const noexcept;
+
+        QChar front() const;
+        QChar back()  const;
+
+        std::vector<core::data::range_format> getStyles() const;
+
+        [[nodiscard]] FStringView trimmed() const;
+
+        [[nodiscard]] FStringView mid(qsizetype _offset) const;
+        [[nodiscard]] FStringView mid(qsizetype _offset, qsizetype _size) const;
+        [[nodiscard]] FStringView left(qsizetype _size) const;
+        [[nodiscard]] FStringView right(qsizetype _size) const;
+
+        void chop(int _n);
+
+        QChar at(int _pos) const;
+        QChar lastChar() const;
+        int indexOf(QChar _char, qsizetype _from = 0) const;
+        int indexOf(QStringView _string, qsizetype _from = 0) const;
+        int lastIndexOf(QChar _char) const;
+        bool startsWith(QStringView _prefix) const;
+        bool endsWith(QStringView _prefix) const;
+
+        FString toFString() const;
+
+        //! Check if entire view has these format types
+        bool isAnyOf(FormatTypes _types) const noexcept;
+
+        bool operator==(QStringView _other) const { return string() == _other; }
+        bool operator!=(QStringView _other) const { return !operator==(_other); }
+
+        QStringView string() const;
+        QString toString() const;
+        FStringView sourceView() const noexcept;
+        core::data::range sourceRange() const noexcept;
+        core::data::range getRangeOf(const FString& _word) const;
+
+        //! Replace plain string and extend range if it occupied entire string
+        FString replaceByString(QStringView _newString, bool _keepMentionFormat = true) const;
+
+        bool tryToAppend(FStringView _other);
+        bool tryToAppend(QChar _ch);
+        bool tryToAppend(QStringView _text);
+
+    private:
+        FStringView(std::shared_ptr<SharedData> _shared, int _offset, int _size);
+
+        [[nodiscard]] core::data::range cutRangeToFitView(core::data::range _range) const;
+        [[nodiscard]] core::data::format getFormat() const;
+        std::shared_ptr<SharedData> sharedData() const;
+
+    private:
+        mutable std::weak_ptr<SharedData> data_;
+        mutable int offset_;
+        mutable int size_;
+        
+        friend class FString;
+    };
+
     class FString
     {
         friend class FStringView;
@@ -54,7 +136,6 @@ namespace Data
         };
 
     public:
-        struct SharedData;
         using SharedDataPtr = std::shared_ptr<SharedData>;
 
         FString();
@@ -125,148 +206,5 @@ namespace Data
 
     private:
         std::shared_ptr<SharedData> sharedData_;
-    };
-
-    //! NB: FormattedStringView doesn't take care of keeping any ownership
-    class FStringView
-    {
-        FStringView(FString::SharedDataPtr _shared, int _offset, int _size);
-    public:
-        using const_iterator = QString::const_iterator;
-        using const_reverse_iterator = QString::const_reverse_iterator;
-
-        FStringView() noexcept;
-        FStringView(const FStringView&) = default;
-        FStringView(FStringView&&) noexcept = default;
-        FStringView(const FString& _string, int _offset = 0, int _size = -1);
-        ~FStringView() = default;
-
-        FStringView& operator=(const FStringView& _other) = default;
-        FStringView& operator=(FStringView&& _other) noexcept = default;
-
-        bool hasFormatting() const noexcept;
-        bool containsFormat(core::data::format_type _type) const;
-        bool isEmpty() const noexcept;
-        int size() const noexcept;
-
-        QChar front() const;
-        QChar back()  const;
-
-        std::vector<core::data::range_format> getStyles() const;
-
-        [[nodiscard]] FStringView trimmed() const;
-
-        [[nodiscard]] FStringView mid(qsizetype _offset) const;
-        [[nodiscard]] FStringView mid(qsizetype _offset, qsizetype _size) const;
-        [[nodiscard]] FStringView left(qsizetype _size) const;
-        [[nodiscard]] FStringView right(qsizetype _size) const;
-
-        void chop(int _n);
-
-        QChar at(int _pos) const;
-        QChar lastChar() const;
-        int indexOf(QChar _char, qsizetype _from = 0) const;
-        int indexOf(QStringView _string, qsizetype _from = 0) const;
-        int lastIndexOf(QChar _char) const;
-        bool startsWith(QStringView _prefix) const;
-        bool endsWith(QStringView _prefix) const;
-
-        FString toFString() const;
-
-        //! Check if entire view has these format types
-        bool isAnyOf(FormatTypes _types) const noexcept;
-
-        bool operator==(QStringView _other) const { return string() == _other; }
-        bool operator!=(QStringView _other) const { return !operator==(_other); }
-
-        QStringView string() const;
-        QString toString() const;
-        FStringView sourceView() const noexcept;
-        core::data::range sourceRange() const noexcept;
-        core::data::range getRangeOf(const FString& _word) const;
-
-        //! Replace plain string and extend range if it occupied entire string
-        FString replaceByString(QStringView _newString, bool _keepMentionFormat = true) const;
-
-        bool tryToAppend(FStringView _other);
-        bool tryToAppend(QChar _ch);
-        bool tryToAppend(QStringView _text);
-
-    private:
-        [[nodiscard]] core::data::range cutRangeToFitView(core::data::range _range) const;
-        [[nodiscard]] core::data::format getFormat() const;
-
-    private:
-        FString::SharedDataPtr sharedData() const;
-
-    private:
-        mutable std::weak_ptr<FString::SharedData> data_;
-        mutable int offset_;
-        mutable int size_;
-    };
-}
-
-        FStringView& operator=(const FStringView& _other) = default;
-        FStringView& operator=(FStringView&& _other) noexcept = default;
-
-        bool hasFormatting() const noexcept;
-        bool containsFormat(core::data::format_type _type) const;
-        bool isEmpty() const noexcept;
-        int size() const noexcept;
-
-        QChar front() const;
-        QChar back()  const;
-
-        std::vector<core::data::range_format> getStyles() const;
-
-        [[nodiscard]] FStringView trimmed() const;
-
-        [[nodiscard]] FStringView mid(qsizetype _offset) const;
-        [[nodiscard]] FStringView mid(qsizetype _offset, qsizetype _size) const;
-        [[nodiscard]] FStringView left(qsizetype _size) const;
-        [[nodiscard]] FStringView right(qsizetype _size) const;
-
-        void chop(int _n);
-
-        QChar at(int _pos) const;
-        QChar lastChar() const;
-        int indexOf(QChar _char, qsizetype _from = 0) const;
-        int indexOf(QStringView _string, qsizetype _from = 0) const;
-        int lastIndexOf(QChar _char) const;
-        bool startsWith(QStringView _prefix) const;
-        bool endsWith(QStringView _prefix) const;
-
-        FString toFString() const;
-
-        //! Check if entire view has these format types
-        bool isAnyOf(FormatTypes _types) const noexcept;
-
-        bool operator==(QStringView _other) const { return string() == _other; }
-        bool operator!=(QStringView _other) const { return !operator==(_other); }
-
-        QStringView string() const;
-        QString toString() const;
-        FStringView sourceView() const noexcept;
-        core::data::range sourceRange() const noexcept;
-        core::data::range getRangeOf(const FString& _word) const;
-
-        //! Replace plain string and extend range if it occupied entire string
-        FString replaceByString(QStringView _newString, bool _keepMentionFormat = true) const;
-
-        bool tryToAppend(FStringView _other);
-        bool tryToAppend(QChar _ch);
-        bool tryToAppend(QStringView _text);
-
-    private:
-        [[nodiscard]] core::data::range cutRangeToFitView(core::data::range _range) const;
-        [[nodiscard]] core::data::format getFormat() const;
-
-    private:
-        FString::SharedDataPtr sharedData() const;
-
-    private:
-        mutable std::weak_ptr<FString::SharedData> data_;
-        mutable int offset_;
-        mutable int size_;
     };
 }
