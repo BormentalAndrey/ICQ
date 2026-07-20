e04527cc04bcaedb08cdd6a4cc1608394b3fda2b
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-20 22:11:42 +0300

    Update AppModule.kt

--- app/src/main/java/com/nexus/player/di/AppModule.kt
+++ app/src/main/java/com/nexus/player/di/AppModule.kt
@@ -5,8 +5,6 @@ import com.nexus.player.NexusApplication
 import com.nexus.player.data.local.PreferencesManager
 import com.nexus.player.data.repository.MediaRepository
 import com.nexus.player.player.audio.EqualizerEngine
-import com.nexus.player.player.audio.FftAnalyzer
-import com.nexus.player.player.audio.KaraokeProcessor
 import com.nexus.player.player.core.CorruptedFileHandler
 import com.nexus.player.player.video.SubtitleRenderer
 
@@ -18,8 +16,6 @@ object AppModule {
     private val mediaRepository: MediaRepository by lazy { MediaRepository(context) }
     private val corruptedFileHandler: CorruptedFileHandler by lazy { CorruptedFileHandler() }
     private val equalizerEngine: EqualizerEngine by lazy { EqualizerEngine() }
-    private val fftAnalyzer: FftAnalyzer by lazy { FftAnalyzer() }
-    private val karaokeProcessor: KaraokeProcessor by lazy { KaraokeProcessor() }
     private val subtitleRenderer: SubtitleRenderer by lazy { SubtitleRenderer(context) }
     
     fun initialize(application: NexusApplication) {
@@ -30,7 +26,5 @@ object AppModule {
     fun provideMediaRepository(): MediaRepository = mediaRepository
     fun provideCorruptedFileHandler(): CorruptedFileHandler = corruptedFileHandler
     fun provideEqualizerEngine(): EqualizerEngine = equalizerEngine
-    fun provideFftAnalyzer(): FftAnalyzer = fftAnalyzer
-    fun provideKaraokeProcessor(): KaraokeProcessor = karaokeProcessor
     fun provideSubtitleRenderer(): SubtitleRenderer = subtitleRenderer
 }
