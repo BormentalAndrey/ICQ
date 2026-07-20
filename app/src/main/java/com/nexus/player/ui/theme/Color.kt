7f98cbc83b2dba25de1129d9f636feecf84d71e6
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-20 21:01:50 +0300

    Create Color.kt

---
+++ app/src/main/java/com/nexus/player/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.nexus.player.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object NexusColors {
+    val BlackBackground = Color(0xFF0D0D0D)
+    val DarkGrey = Color(0xFF1A1A1A)
+    val MediumGrey = Color(0xFF2A2A2A)
+    val NeonPink = Color(0xFFFF007F)
+    val Cyan = Color(0xFF00F0FF)
+    val Purple = Color(0xFF7A00FF)
+    val NeonGreen = Color(0xFF00FF41)
+    val ElectricBlue = Color(0xFF0066FF)
+    val BloodRed = Color(0xFFFF073A)
+    val GoldenYellow = Color(0xFFFFD700)
+    val White = Color(0xFFE0E0E0)
+    val GlassWhite = Color(0x1AFFFFFF)
+    val GlassBlack = Color(0xB30D0D0D)
+    val NeonPinkTransparent = Color(0x40FF007F)
+    val CyanTransparent = Color(0x4000F0FF)
+    val PurpleTransparent = Color(0x407A00FF)
+}
