9fd1c06d4bbdf0970fb4319f51d73edf5068d1f9
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-20 21:58:36 +0300

    Update AppModule.kt

--- app/src/main/java/com/nexus/player/di/AppModule.kt
+++ app/src/main/java/com/nexus/player/di/AppModule.kt
@@ -13,13 +13,14 @@ import com.nexus.player.player.video.SubtitleRenderer
 object AppModule {
     
     private lateinit var context: Context
-    private val preferencesManager by lazy { PreferencesManager(context) }
-    private val mediaRepository by lazy { MediaRepository(context) }
-    private val corruptedFileHandler by lazy { CorruptedFileHandler() }
-    private val equalizerEngine by lazy { EqualizerEngine() }
-    private val fftAnalyzer by lazy { FftAnalyzer() }
-    private val karaokeProcessor by lazy { KaraokeProcessor() }
-    private val subtitleRenderer by lazy { SubtitleRenderer(context) }
+    
+    private val preferencesManager: PreferencesManager by lazy { PreferencesManager(context) }
+    private val mediaRepository: MediaRepository by lazy { MediaRepository(context) }
+    private val corruptedFileHandler: CorruptedFileHandler by lazy { CorruptedFileHandler() }
+    private val equalizerEngine: EqualizerEngine by lazy { EqualizerEngine() }
+    private val fftAnalyzer: FftAnalyzer by lazy { FftAnalyzer() }
+    private val karaokeProcessor: KaraokeProcessor by lazy { KaraokeProcessor() }
+    private val subtitleRenderer: SubtitleRenderer by lazy { SubtitleRenderer(context) }
     
     fun initialize(application: NexusApplication) {
         context = application.applicationContext
